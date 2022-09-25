f81a60adc65ca50988cb3165af88c98ae5628e9a
Varga <vargazsigmond02@gmail.com>
2022-09-25 19:34:18 +0200

    bezaros

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import './main.css';
+import '/index.html';
 
+window.onload = function(){
+    document.getElementById('close').onclick = function(){
+        this.parentNode.parentNode.remove();
+        return false;
+    };
+};
